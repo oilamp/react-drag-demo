4845bbb958901f85f0ada7de5a0f6bd577c4cd38
jian.wang <jian.wang@cpoao.com>
2020-04-04 15:40:37 +0800

    fix:编译提交

---
+++ build/precache-manifest.662149c6cef899f9ee9e053b2932189d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26803052da6e7f395b498293f0fd5d5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99544ba81e7ad0599f28",
+    "url": "/static/css/main.c6904d74.chunk.css"
+  },
+  {
+    "revision": "a686e410f739f2748a0b",
+    "url": "/static/js/2.566e7fa5.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.566e7fa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99544ba81e7ad0599f28",
+    "url": "/static/js/main.2a640eea.chunk.js"
+  },
+  {
+    "revision": "e5d8b17672561da45b22",
+    "url": "/static/js/runtime-main.c7afc562.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ad0d6e3b0f925753d5b4205f59b11129",
+    "url": "/static/media/text.ad0d6e3b.svg"
+  }
+]);
